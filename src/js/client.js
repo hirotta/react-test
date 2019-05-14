f9fcc68f8665e4a888f4e0bd336fdf87a9272f28
nekopanda <kouhiro3782@yahoo.co.jp>
2019-05-14 14:43:03 +0900

    done

--- src/js/client.js
+++ src/js/client.js
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-class Layout extends React.Component {
-    render(){
-        let name = "nekopanda"
-        return(
-            <h1>hello {name}</h1>
-        );
-    }
-}
+import Layout from "./components/Layout"
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
